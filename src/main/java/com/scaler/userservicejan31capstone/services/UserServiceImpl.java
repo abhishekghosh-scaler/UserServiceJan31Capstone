e483c5fcaab2141dc6308a110510f8726afce495
Abhishek Ghosh <abhishek.ghosh_1@scaler.com>
2025-03-28 23:25:09 +0530

    Adding Kafka integration

--- src/main/java/com/scaler/userservicejan31capstone/services/UserServiceImpl.java
+++ src/main/java/com/scaler/userservicejan31capstone/services/UserServiceImpl.java
@@ -1,10 +1,14 @@
 package com.scaler.userservicejan31capstone.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.scaler.userservicejan31capstone.dtos.SendEmailDto;
 import com.scaler.userservicejan31capstone.models.Token;
 import com.scaler.userservicejan31capstone.models.User;
 import com.scaler.userservicejan31capstone.repositories.TokenRepository;
 import com.scaler.userservicejan31capstone.repositories.UserRepository;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -20,14 +24,20 @@ public class UserServiceImpl implements UserService
     UserRepository userRepository;
     BCryptPasswordEncoder bCryptPasswordEncoder;
     TokenRepository tokenRepository;
+    KafkaTemplate<String, String> kafkaTemplate;
+    ObjectMapper objectMapper;
 
     public UserServiceImpl(UserRepository userRepository,
                            BCryptPasswordEncoder bcryptPasswordEncoder,
-                           TokenRepository tokenRepository)
+                           TokenRepository tokenRepository,
+                           KafkaTemplate<String, String> kafkaTemplate,
+                           ObjectMapper objectMapper)
     {
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bcryptPasswordEncoder;
         this.tokenRepository = tokenRepository;
+        this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
     }
 
     @Override
@@ -36,6 +46,22 @@ public class UserServiceImpl implements UserService
         user.setName(name);
         user.setEmail(email);
         user.setPassword(bCryptPasswordEncoder.encode(password));
+
+        SendEmailDto sendEmailDto = new SendEmailDto();
+        sendEmailDto.setFrom("ghosh.abhishek18459@gmail.com");
+        sendEmailDto.setSubject("User Registration");
+        sendEmailDto.setBody("Hello, " + name + "!");
+        sendEmailDto.setTo(email);
+
+        String sendEmailDtoString;
+
+        try {
+            sendEmailDtoString = objectMapper.writeValueAsString(sendEmailDto);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        kafkaTemplate.send("sendEmail", sendEmailDtoString);
 
         return userRepository.save(user);
     }
